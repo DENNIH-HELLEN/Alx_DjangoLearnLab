58c7c55ec1e7b827c87e06c5ef75de4975726084
Dennis Mwangi <mwangidennis771414@gmail.com>
2024-11-17 18:02:21 +0300

    Introduction to DJango

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,3 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
-    
---
+++ Introduction_to_Django/README.md
@@ -0,0 +1,1 @@
+# Solutions to introduction to Django
